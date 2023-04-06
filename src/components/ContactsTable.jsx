5a8ee0041a4bda5297f7eb024ec8dd615f38d69a
Darius <darius.draksas244@gmail.com>
2023-04-06 14:02:13 +0300

    update

--- src/components/ContactsTable.jsx
+++ src/components/ContactsTable.jsx
@@ -16,44 +16,38 @@ const ContactsTable = () => {
                 id: doc.id,
                 ...doc.data()
             }));
-        console.log(tempQuestion)
     setQuestions(tempQuestion)
-    console.log(questions)
     }
 
     useEffect(()=>{
         getDataFromFirestore()
-    }, [])
+    }, [questions])
 
 
   return (
     <Table striped>
-            <thead>
-                <tr>
-                    <th>Nr.</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Question</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    questions?.map((question, index)=>(
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{question.client_name}</td>
-                            <td>{question.client_email}</td>
-                            <td>{question.client_message}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </Table>
+        <thead>
+            <tr>
+                <th>Nr.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Question</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                questions?.map((question, index)=>(
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{question.client_name}</td>
+                        <td>{question.client_email}</td>
+                        <td>{question.client_message}</td>
+                    </tr>
+                ))
+            }
+        </tbody>
+    </Table>
   )
 }
 
 export default ContactsTable
-
-
-
-
